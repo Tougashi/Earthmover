bea2dbe0f5e53ab81764610458a2a6071768553f
Tougashiro <muhammadadryan505@gmail.com>
2024-01-09 14:56:52 +0700

    Relationship Role  & User

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'password' => 'hashed',
     ];
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
 }
